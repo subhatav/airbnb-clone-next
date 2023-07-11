6322de46d814a63363e8b0bda9601c0b3103c3b8
Subhatav Dhali <subhatav@gmail.com>
2023-07-11 19:02:51 +0530

    Create the Button component in the `Button` TSX file

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -1,14 +1,41 @@
+import { IconType } from "react-icons";
+
+export interface ClientOnlyProps {
+  children: React.ReactNode;
+}
+
 export interface AvatarProps {
   src: string | null | undefined;
 }
 
 export interface MenuItemProps {
-  onClick: () => void;
-
   label: string;
+  onClick: () => void;
   isOptional?: boolean;
 }
 
 export interface ContainerProps {
   children: React.ReactNode;
+}
+
+export interface ButtonProps {
+  label: string;
+  onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
+  disabled?: boolean;
+  outline?: boolean;
+  small?: boolean;
+  icon?: IconType;
+}
+
+export interface ModalProps {
+  isOpen?: boolean;
+  onClose: () => void;
+  onSubmit: () => void;
+  title?: string;
+  body?: React.ReactElement;
+  footer?: React.ReactElement;
+  actionLabel: string;
+  disabled?: boolean;
+  secondaryAction?: () => void;
+  secondaryActionLabel?: string;
 }
